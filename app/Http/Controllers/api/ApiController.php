6bd97298a79576c0617ed17991bf16746ffcb9a6
Jad-Bel <jadthegamer06@gmail.com>
2025-03-19 10:59:57 +0000

    FEAT: initialized the api controller (auth controller) for the jwt

---
+++ app/Http/Controllers/api/ApiController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    // Register (POST, fromdata)
+    public function register ()
+    {
+
+    }
+
+    // login (POST, fromdata)
+    public function login ()
+    {
+
+    }
+
+    // profile (GET)
+    public function profile ()
+    {
+
+    }
+
+    // refresh token (GET)
+    public function refreshToken ()
+    {
+
+    }
+
+    // logout (GET)
+    public function logout ()
+    {
+
+    }
+}
