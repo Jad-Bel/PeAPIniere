4e4e52c930e6a344a610dfd11e40efbae7fcd28c
Jad-Bel <jadthegamer06@gmail.com>
2025-03-19 13:39:54 +0000

    FEAT: implemented logic for registration and testing it

--- app/Http/Controllers/api/ApiController.php
+++ app/Http/Controllers/api/ApiController.php
@@ -2,15 +2,35 @@
 
 namespace App\Http\Controllers\api;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 
 class ApiController extends Controller
 {
     // Register (POST, fromdata)
-    public function register ()
+    public function register (Request $request)
     {
+        // data validation
+        $request->validate([
+            "name" => "required",
+            "email" => "required|email|unique:users",
+            "password" => "required|confirmed"
+        ]);
 
+        // Create user
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'passowrd' => Hash::make($request->password)
+        ]);
+
+        // Response
+        return response()->json([
+            'status' => true,
+            'message' => 'user created successfully',
+        ]);
     }
 
     // login (POST, fromdata)
